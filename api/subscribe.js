1a14f85754d0feffe3d6ea56589aef5325b31fde
irawo/star <irawo10@gmail.com>
2026-02-20 13:34:36 -0500

    confirmation message

--- api/subscribe.js
+++ api/subscribe.js
@@ -68,7 +68,7 @@ export default async function handler(req, res) {
       });
     }
 
-    return res.status(200).json({ message: "Success" });
+    return res.status(200).json({ message: "You're on the list, thank you for joining us!" });
 
   } catch (error) {
     console.error(error);
